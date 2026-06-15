a50ec7c9d620e0cd1ceeb800fbce709abfc13720
carline <carline@v3x.email>
2026-06-15 15:30:15 +0200

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,13 @@
 export function Navbar() {
     return (
-        <div className="w-full bg-[#788a1d] h-14 text-white px-4">
-            <div className="w-full max-w-7xl mx-auto h-full flex items-center">
-                Geerts Groen
+        <div className="w-full bg-[#788a1d] h-28 text-white px-4">
+            <div className="w-full max-w-7xl mx-auto h-full flex items-center gap-4">
+                <div className="aspect-square h-20">
+                    <img src="/public/logo_opacity_partial.webp" alt="logo" className="w-full h-full object-contain " />
+                </div>
+                <div className="text-2xl ">
+                    Geerts Groen
+                </div>
             </div>
         </div>
     );
