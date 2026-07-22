e70f67eae7fb81d2b5d6b4afd83c0342d74cfaf8
carline-sh <carline@v3x.email>
2026-07-22 22:53:35 +0200

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import { email, companyName, kvk, phone } from "../../content/company.json";
 
 export function Footer() {
     return (
-        <div className="relative z-20 w-full -mt-6" style={{ backgroundImage: `url(/public/images/grondvoorvoeter.png)` }}>
+        <div className="relative z-20 w-full -mt-6" style={{ backgroundImage: `url(/images/grondvoorvoeter.png)` }}>
             <div className="w-full max-w-7xl mx-auto min-h-48 md:min-h-64 text-white pt-12 px-4 flex justify-between">
                 <div className="">
                     <div className="h-20">
-                        <img src="/public/images/LogoLetterworkWhite.svg" alt={companyName} title={companyName} className="h-20 object-contain " />
+                        <img src="/images/LogoLetterworkWhite.svg" alt={companyName} title={companyName} className="h-20 object-contain " />
                     </div>
                     <div className="pl-4 pt-2">
                         <div className="flex items-center gap-2">
