96e77763ed06cd028a40cab7a0580b093faa9e58
carline-sh <carline@v3x.email>
2026-07-23 15:11:06 +0200

    make responsive

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -11,11 +11,11 @@ export function Works() {
                     <h2 className="text-3xl md:text-4xl miranda-sans-bold text-white">
                         {heading}
                     </h2>
-                    <div className="grid grid-flow-col gap-4 grid-rows-3">
+                    <div className="grid grid-cols-1 gap-4 md:grid-cols-none md:grid-flow-col md:grid-rows-3">
                         {servicesList.map((work) => (
                             <div
                                 key={work.serviceName}
-                                className="flex flex-col gap-3 bg-white/20 text-white rounded-md py-4 px-6"
+                                className="flex min-w-0 flex-col gap-3 bg-white/20 text-white rounded-md py-4 px-4 md:px-6"
                             >
                                 <span className="text-lg md:text-xl font-bold leading-snug">
                                     {work.serviceName}
