22a4f22ec4647a0c62fe8c77597dc449ff32fa5c
carline <carline@v3x.email>
2026-07-29 14:10:59 +0200

    update mobile

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -11,7 +11,7 @@ export function Works() {
                     <h2 className="text-3xl md:text-4xl miranda-sans-bold text-white">
                         {heading}
                     </h2>
-                    <div className="grid grid-cols-1 gap-4 md:grid-cols-none md:grid-flow-col md:grid-rows-3">
+                    <div className="grid gap-4 md:grid-cols-none md:grid-flow-col md:grid-rows-3 grid-cols-2">
                         {servicesList.map((work) => (
                             <div
                                 key={work.serviceName}
