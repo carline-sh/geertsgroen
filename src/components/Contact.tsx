7fe7228e8309f2ee00495576f7e333f258521d2a
carline <carline@v3x.email>
2026-07-29 14:14:59 +0200

    prettify

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,11 @@
 import { FaWhatsapp } from "react-icons/fa";
-import { whatsappNumber, email, phone} from "../../content/company.json";
+import { whatsappNumber, email, phone } from "../../content/company.json";
 import { heading, body, whatsappText, emailText, phoneText } from '../../content/contact.json';
 import { FiMail, FiPhone } from "react-icons/fi";
 
 export function Contact() {
     return (
-        <div id="contact" className="w-full scroll-mt-28 relative bg-[#F5F0E6] pb-12 pt-12">
+        <div id="contact" className="w-full scroll-mt-28 relative bg-[#F5F0E6] pb-12 pt-12 px-6 md:px-0">
             {/* <div className="absolute left-0 right-0 -top-8 md:-top-20 z-10 pointer-events-none">
                 <div className="max-w-7xl mx-auto px-4 md:px-8">
                      <div className="max-w-4xl mx-auto">
@@ -22,16 +22,16 @@ export function Contact() {
             <div className="relative max-w-6xl mx-auto rounded-4xl bg-[#FDC005] wrap-break-word py-12 text-white flex items-center justify-center px-12 mb-12">
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
                     <div className="lg:col-span-2 space-y-4">
-                    <h2 className="text-3xl md:text-4xl miranda-sans-bold">{heading}</h2>
-                    <p className="text-lg opacity-90">{body}</p>
-                        </div>
+                        <h2 className="text-3xl md:text-4xl miranda-sans-bold">{heading}</h2>
+                        <p className="text-lg opacity-90">{body}</p>
+                    </div>
                     <div className="space-y-4">
                         <a
                             href={"https://wa.me/" + whatsappNumber}
                             className="font-sans flex items-center gap-3 bg-[#2e5713] text-white font-bold px-6 py-3 rounded-xl shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-200"
                             target="_blank"
                             rel="noopener noreferrer"
-                            >
+                        >
                             <FaWhatsapp className="text-xl" />
                             <span>{whatsappText}</span>
                         </a>
